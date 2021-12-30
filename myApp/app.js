68bde8d7a6e87a130e195febf6d327a8c40b7a28
Hossam Elfar <elfarhossam@gmail.com>
2021-12-31 01:23:48 +0200

    Done

--- myApp/app.js
+++ myApp/app.js
@@ -339,9 +339,6 @@ app.post("/",function(req,res){
       res.redirect('/')
     })
   })  
-if(process.env.PORT){
-  app.listen(procces.env.PORT,function(){console.log('Server Started')})
-}
-else {
-  app.listen(3000,function(){console.log('Server Started on port 3000')})
-}
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
